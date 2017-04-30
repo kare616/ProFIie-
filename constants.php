7b082426b2acf4362293b7d8865ff09fb296a788
kare616 <karesblog@gmail.com>
2017-04-30 07:25:00 +0530

    Update constants.php

--- constants.php
+++ constants.php
@@ -3,8 +3,8 @@
 * @author Tomiwa Adefokun <tomiwa.adefokun@reinsys.com>
 * @copyright 2016-2017 Reinsys
 */
-define('FB_APP_ID', '');
-define('FB_APP_SECRET', '');
+define('FB_APP_ID', '661997940665897');
+define('FB_APP_SECRET', '65cd9fb27db46084ad2932c246cf64fd');
 define('UPLOADED_IMAGE_DESTINATION', 'originals/');
 define('PROCESSED_IMAGE_DESTINATION', 'images/');
 define('FILTER_BASE_URL', '');
